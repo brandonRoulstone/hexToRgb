44087b9ae940aa518f8c48c83e4f4d8132ed771b
Brandon Roulstone <brandonroulstone71@gmail.com>
2023-11-27 14:14:13 +0200

    2nd commit

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ let btnCalculate = document.getElementById("calculate");
 let res = document.getElementById("res");
 
 function hexToRgb(hex) {
-    // Remove # if present
+
     hex = hex.replace('#', '');
 
-    // Ensure the hex value is a valid length (3 or 6 characters)
     if (hex.length !== 3 && hex.length !== 6) {
         alert("Invalid hex color")
         throw new Error('server reponded with a 403');
     }
 
     // Expand shorthand hex if it's in the format #RGB
+    // For hex values with the length of three
+    // Will add up first index till the last index of 3rd
     if (hex.length === 3) {
         hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
     }
 
-    // Parse hex to decimal values for R, G, B
     let red = parseInt(hex.substring(0, 2), 16);
     let green = parseInt(hex.substring(2, 4), 16);
     let blue = parseInt(hex.substring(4, 6), 16);
